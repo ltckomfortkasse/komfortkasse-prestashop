ed50d001bad74ec75c2933426a1322856cf38e68
machajdik <manuel.machajdik@gmail.com>
2016-11-17 16:42:24 +0100

    changed string casts do double quotes

--- ltckomfortkasse/lib/Komfortkasse_Order.php
+++ ltckomfortkasse/lib/Komfortkasse_Order.php
@@ -8,7 +8,7 @@
  * status: data type according to the shop system
  * delivery_ and billing_: _firstname, _lastname, _company, _street, _postcode, _city, _countrycode
  * products: an Array of item numbers
- * @version 1.4.6-prestashop
+ * @version 1.4.5-prestashop
  */
 
 $order_extension = false;
@@ -48,11 +48,11 @@ class Komfortkasse_Order
 				FROM ' . (string)_DB_PREFIX_ . 'orders o
 				WHERE 0 ';
         if ($use_prepayment)
-            $sql .= ' or (o.current_state in (' . (int)$status_prepayment . ') and o.module in (' . (string)pSQL((string)self::quote((string)$methods_prepayment)) . '))';
+            $sql .= ' or (o.current_state in (' . (int)$status_prepayment . ') and o.module in (' . (string)pSQL((string)self::quote("$methods_prepayment")) . '))';
         if ($use_invoice)
-            $sql .= ' or (o.current_state in (' . (int)$status_invoice . ') and o.module in (' . (string)pSQL((string)self::quote((string)$methods_invoice)) . '))';
+            $sql .= ' or (o.current_state in (' . (int)$status_invoice . ') and o.module in (' . (string)pSQL((string)self::quote("$methods_invoice")) . '))';
         if ($use_cod)
-            $sql .= ' or (o.current_state in (' . (int)$status_cod . ') and o.module in (' . (string)pSQL((string)self::quote((string)$methods_cod)) . '))';
+            $sql .= ' or (o.current_state in (' . (int)$status_cod . ') and o.module in (' . (string)pSQL((string)self::quote("$methods_cod")) . '))';
 
         $result = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
 
@@ -266,7 +266,7 @@ class Komfortkasse_Order
     public static function getInvoicePdf($invoiceNumber)
     {
         // get newest id with that number (for numbers that reset every year)
-        $sql = 'SELECT id_order_invoice FROM ' . (string)_DB_PREFIX_ . 'order_invoice o WHERE number=' . (string)pSQL((string)$invoiceNumber) . ' order by date_add desc limit 0, 1';
+        $sql = 'SELECT id_order_invoice FROM ' . (string)_DB_PREFIX_ . 'order_invoice o WHERE number=' . (string)pSQL("$invoiceNumber") . ' order by date_add desc limit 0, 1';
         $result = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
         if (empty($result))
             return;
