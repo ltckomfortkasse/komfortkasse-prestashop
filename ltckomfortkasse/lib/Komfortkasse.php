9e236ae412ad27714acb207188aa66e92b3ac10c
machajdik <manuel.machajdik@gmail.com>
2019-07-02 13:40:49 +0200

    1.7.15

--- ltckomfortkasse/lib/Komfortkasse.php
+++ ltckomfortkasse/lib/Komfortkasse.php
@@ -8,7 +8,7 @@ require_once 'Komfortkasse_Order.php';
  */
 class Komfortkasse
 {
-    const PLUGIN_VER = '1.7.14';
+    const PLUGIN_VER = '1.7.15';
     const MAXLEN_SSL = 117;
     const LEN_MCRYPT = 16;
 
